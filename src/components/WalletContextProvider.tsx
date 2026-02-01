0a7b7bd944513f5711b224485a3263f68ca253bb
ShivamSoni <ss28022802@gmail.com>
2026-02-02 02:03:17 +0530

    Wallet: Enable autoConnect for improved user experience

--- src/components/WalletContextProvider.tsx
+++ src/components/WalletContextProvider.tsx
@@ -31,7 +31,7 @@ export const WalletContextProvider: FC<{ children: ReactNode }> = ({ children })
 
     return (
         <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect={false}>
+            <WalletProvider wallets={wallets} autoConnect={true}>
                 <WalletModalProvider>
                     {children}
                 </WalletModalProvider>
